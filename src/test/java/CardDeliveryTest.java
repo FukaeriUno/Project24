7f88d170a8f8640f99a6216f62bb8f426af6058d
Plotnikova Anastasiya <nast_19980728@mail.ru>
2022-03-08 13:04:49 +0300

    Add currentDatePlusThree func

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,22 +1,36 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-
     @Test
     public void shouldSendCardDeliveryForm() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date today = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(today);
+
+        c.add(Calendar.DATE, 3);
+
+        Date currentDatePlusThree = c.getTime();
+
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue("11.12.2023");
+        $("[data-test-id=date] input").setValue(String.valueOf(currentDatePlusThree));
         $("[data-test-id=name] input").setValue("Галина Андреева");
         $("[data-test-id=phone] input").setValue("+79256341589");
         $(By.className("checkbox__box")).click();

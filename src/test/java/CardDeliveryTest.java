f6f0291b41d63e998dec6f3d40b19919cf07d9c7
Plotnikova Anastasiya <nast_19980728@mail.ru>
2022-03-10 22:00:11 +0300

    Fix problems

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -16,25 +18,26 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
+    String planningDate = generateDate(4);
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     public void shouldSendCardDeliveryForm() {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        Date today = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(today);
-
-        c.add(Calendar.DATE, 3);
-
-        Date currentDatePlusThree = c.getTime();
 
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue(String.valueOf(currentDatePlusThree));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(String.valueOf(planningDate));
         $("[data-test-id=name] input").setValue("Галина Андреева");
         $("[data-test-id=phone] input").setValue("+79256341589");
         $(By.className("checkbox__box")).click();
         $(".button__text").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 }
